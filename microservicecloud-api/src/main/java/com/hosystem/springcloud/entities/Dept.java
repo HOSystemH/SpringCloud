9e06fe2791b0dba6d4d002fb51f17185500ca465
pluto <pluto@aliyun.com>
2020-11-05 20:29:08 +0800

    cnm

---
+++ microservicecloud-api/src/main/java/com/hosystem/springcloud/entities/Dept.java
@@ -0,0 +1,28 @@
+package com.hosystem.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+//entity  --orm-- db_table
+// orm 类表关系映射 https://baike.baidu.com/item/%E5%AF%B9%E8%B1%A1%E5%85%B3%E7%B3%BB%E6%98%A0%E5%B0%84/311152?fromtitle=ORM&fromid=3583252&fr=aladdin
+public class Dept implements Serializable{  //必须序列化
+    private Long  deptno;   //主键
+    private String  dname;   //部门名称
+    private String  db_source;//来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+    public Dept(String dname) {
+        String s="";
+        System.out.println(s);
+        this.dname = dname;
+    }
+
+}
---
+++ microservicecloud-provider-dept-8001/src/main/java/com/hosystem/springcloud/service/DeptService.java
@@ -0,0 +1,11 @@
+package com.hosystem.springcloud.service;
+import java.util.List;
+
+import com.hosystem.springcloud.entities.Dept;
+
+public interface DeptService
+{
+    public boolean add(Dept dept);
+    public Dept    get(Long id);
+    public List<Dept> list();
+}
