297608c8770b45f81c48aa19eaa01b5ad478743f
pluto <pluto@aliyun.com>
2020-11-07 11:11:34 +0800

    DeptProvider8001_App注册到EurekaServer上

--- microservicecloud-provider-dept-8001/src/main/java/com/hosystem/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hosystem/springcloud/DeptProvider8001_App.java
@@ -2,13 +2,14 @@ package com.hosystem.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class DeptProvider8001_App
 {
     public static void main(String[] args)
     {
-        System.out.println("1");
         SpringApplication.run(DeptProvider8001_App.class, args);
     }
 }
