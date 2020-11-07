8b82ffd8841d69a52303e18de68ec241aa412c00
pluto <pluto@aliyun.com>
2020-11-07 15:08:29 +0800

    eureka使用

--- microservicecloud-provider-dept-8001/src/main/java/com/hosystem/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hosystem/springcloud/controller/DeptController.java
@@ -1,14 +1,19 @@
 package com.hosystem.springcloud.controller;
 
-import java.util.List;
 import com.hosystem.springcloud.entities.Dept;
+import java.util.List;
+
 import com.hosystem.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DeptController
@@ -16,21 +21,43 @@ public class DeptController
     @Autowired
     private DeptService service;
 
-    @RequestMapping(value="/dept/add",method= RequestMethod.POST)
+    //注：如果Autowired显示 could not autowired.there is more than one bean of 'DiscoveryClient' type; 使用一下两种方法都行
+    //方法1:那么只需要添加@Qualifier("discoveryClient")即可
+    //方法2::当然也可以修改成 private DiscoveryClient discoveryClient; 但是我们也需要修改discovery()方法中client为discoveryClient
+    @Qualifier("discoveryClient")
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept)
     {
         return service.add(dept);
     }
 
-    @RequestMapping(value="/dept/get/{id}",method=RequestMethod.GET)
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
     public Dept get(@PathVariable("id") Long id)
     {
         return service.get(id);
     }
 
-    @RequestMapping(value="/dept/list",method=RequestMethod.GET)
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list()
     {
         return service.list();
     }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
+
 }
